778387a93a6edb9bb5a440ab459ed58871d51b50
Shravan Jindal <shravanjindal@gmail.com>
2024-12-10 21:54:24 +0530

    functionality added

--- script.js
+++ script.js
@@ -1,61 +1,13 @@
-async function getSongs() {
-    let a = await fetch("./songs/");
-    let response = await a.text();
-    // console.log(response);
-    let element = document.createElement("div");
-
-    element.innerHTML = response;
-    // console.log(element);
-
-    let tiles = element.getElementsByTagName("a");
-    let songinfo = {
-        songs: [],
-        songNames: []
-    }
-    for (let index = 0; index < tiles.length; index++) {
-        if (tiles[index].href.endsWith(".ogg")) {
-            songinfo.songNames.push(tiles[index].getElementsByTagName("span")[0].innerText);
-            songinfo.songs.push(tiles[index].href);
-        }
-    }
-    return songinfo
-}
-
-async function addevents() {
-    let cards1 = document.querySelectorAll(".artist-card")
-    let cards2 = document.querySelectorAll(".playlist-card")
-    for (const card of cards1) {
-        // Add event listeners for mouseover and mouseout
-        card.addEventListener('mouseover', () => {
-            const playButton = card.querySelector('.play');
-            playButton.style.opacity = '1'
-            card.classList.add("hovered")
-        });
-        card.addEventListener('mouseout', () => {
-            const playButton = card.querySelector('.play');
-            playButton.style.opacity = '0'
-            card.classList.remove("hovered")
-        });
-    }
-    for (const card of cards2) {
-        // Add event listeners for mouseover and mouseout
-        card.addEventListener('mouseover', () => {
-            const playButton = card.querySelector('.play');
-            playButton.style.opacity = '1'
-            card.classList.add("hovered")
-        });
-        card.addEventListener('mouseout', () => {
-            const playButton = card.querySelector('.play');
-            playButton.style.opacity = '0'
-            card.classList.remove("hovered")
-        });
-    }
-}
 let audios = []
 let buttons = []
+let newElements = []
 let songtimes = document.querySelector(".songtime").getElementsByTagName("span")
 let songinfo = document.querySelector(".songinfo")
 let circle = document.querySelector(".circle")
+let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0]
+let songbuttons = document.querySelector(".songbuttons").getElementsByTagName("button")
+let last_played;
+let playbtn = songbuttons[1]
 
 // Function to format time (seconds) into mm:ss
 function formatTime(seconds) {
@@ -64,16 +16,11 @@ function formatTime(seconds) {
     return `${mins}:${secs < 10 ? '0' : ''}${secs}`;
 }
 const timer = setInterval(() => {
-    if (audios[audios.length - 1])
+    if (audios[audios.length - 1]) {
         songtimes[0].innerText = formatTime(audios[audios.length - 1].currentTime)
-}, 500)
-const timer2 = setInterval(() => {
-    if (audios[audios.length - 1])
         songtimes[1].innerText = formatTime(audios[audios.length - 1].duration)
-}, 500)
-const timer4 = setInterval(() => {
-    if (audios[audios.length - 1])
-        songinfo.innerText = buttons[buttons.length-1].childNodes[1].innerText
+        songinfo.innerText = buttons[buttons.length - 1].childNodes[1].innerText
+    }
 }, 500)
 
 const timer3 = setInterval(() => {
@@ -89,23 +36,30 @@ const slide = (x, w) => {
     }
 }
 
-async function main() {
-    let songinfo = await getSongs();
-    addevents()
-    let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0]
+async function getSongs(dir) {
+    let a = await fetch(dir);
+    let response = await a.text();
+    // console.log(response);
+    let element = document.createElement("div");
 
-    let songbuttons = document.querySelector(".songbuttons").getElementsByTagName("button")
+    element.innerHTML = response;
+    // console.log(element);
 
-    let last_played;
-    let playbtn = songbuttons[1]
+    let tiles = element.getElementsByTagName("a");
+    let songinfo = {
+        songs: [],
+        songNames: []
+    }
+    for (let index = 0; index < tiles.length; index++) {
+        if (tiles[index].href.endsWith(".mp3")) {
+            songinfo.songNames.push(tiles[index].getElementsByTagName("span")[0].innerText);
+            songinfo.songs.push(tiles[index].href);
+        }
+    }
+    return songinfo
+}
 
-
-    // Add an event listener for beforeunload
-    window.addEventListener('beforeunload', () => {
-        last_played = audios.pop()
-        if (!last_played.paused)
-            last_played.pause()
-    });
+async function playbarController() {
     songbuttons[1].onclick = () => {
         last_played = audios[audios.length - 1]
         let button = buttons[buttons.length - 1]
@@ -120,38 +74,91 @@ async function main() {
             playbtn.childNodes[0].src = "play.svg"
         }
     }
-    songbuttons[0].onclick = () => {
-        last_played = audios[audios.length - 1]
-        if (last_played) {
-            last_played.currentTime = 0
-            buttons[buttons.length - 1].innerHTML = `<img src="music.svg"><p>${buttons[buttons.length - 1].childNodes[1].innerText}</p><img src="play.svg"></img>`
-            last_played.play()
-            playbtn.childNodes[0].src = "play.svg"
-        }
-    }
-    songbuttons[0].ondblclick = () => {
-        last_played = audios[audios.length - 1]
-        let last_played2 = audios[audios.length - 2]
-        let button1 = buttons[buttons.length - 1]
-        let button2 = buttons[buttons.length - 2]
+    // let clickTimer;
 
-        if (last_played2) {
-            last_played.pause()
-            button1.innerHTML = `<img src="music.svg"><p>${button1.childNodes[1].innerText}</p><img src="play2.svg"></img>`
-            button2.innerHTML = `<img src="music.svg"><p>${button2.childNodes[1].innerText}</p><img src="play.svg"></img>`
-            audios.push(last_played2)
-            buttons.push(button2)
-            last_played2.currentTime = 0
-            last_played2.play()
-            playbtn.childNodes[0].src = "play.svg"
+    songbuttons[0].addEventListener('click', () => {
+        // Single-click logic
+        if (audios[audios.length - 1]) {
+            audios[audios.length - 1].currentTime = 0;
+            buttons[buttons.length - 1].innerHTML = `<img src="music.svg"><p>${buttons[buttons.length - 1].childNodes[1].innerText}</p><img src="play.svg"></img>`;
+            audios[audios.length - 1].play();
+            playbtn.childNodes[0].src = "play.svg";
         }
+    });
+    songbuttons[0].addEventListener('dblclick', () => {
+        // Double-click logic
+        let currBtn = buttons[buttons.length - 1];
+        let element;
+        let prev;
+        for (let i = 0; i < newElements.length; i++) {
+            element = newElements[i];
+            if (element.childNodes[0] == currBtn) {
+                prev = (newElements.length + i - 1) % newElements.length;
+                break;
+            }
+        }
+        let audio = new Audio(newElements[prev].childNodes[0].dataset.url);
+        audios[audios.length - 1].pause();
+        buttons[buttons.length - 1].childNodes[2].src = "play2.svg";
+        audios.push(audio);
+        buttons.push(newElements[prev].childNodes[0]);
+        buttons[buttons.length - 1].childNodes[2].src = "play.svg";
+        audio.play();
+        playbtn.childNodes[0].src = "play.svg";
+    });
+    
+// songbuttons[0].onclick = () => {
+//     if (clickTimer) clearTimeout(clickTimer); // Clear any previous single click timer
+//     clickTimer = setTimeout(() => {
+//         clickTimer = null; // Reset the timer
+//         // Single-click logic
+        
+//     }, 300); // Delay to differentiate single and double-clicks
+// };
+
+// songbuttons[0].ondblclick = () => {
+//     if (clickTimer) clearTimeout(clickTimer); // Cancel single-click action
+//     clickTimer = null; // Reset the timer
+//     // Double-click logic
+    
+// };
+
+
+    songbuttons[2].onclick = () => {
+        let currBtn = buttons[buttons.length-1]
+        let element;
+        let next;
+        for (let i = 0; i  < newElements.length; i++) {
+            element = newElements[i];
+            if(element.childNodes[0] == currBtn){
+                next = (i+1)%newElements.length
+                break;
+            }
+        }
+        let audio = new Audio(newElements[next].childNodes[0].dataset.url)
+        audios[audios.length-1].pause()
+        buttons[buttons.length-1].childNodes[2].src = "play2.svg"
+        audios.push(audio)
+        buttons.push(newElements[next].childNodes[0])
+        buttons[buttons.length-1].childNodes[2].src = "play.svg"
+        audio.play()
+        playbtn.childNodes[0].src = "play.svg"
     }
+}
+
+async function main(songinfo) {
+    // Add an event listener for beforeunload
+    window.addEventListener('beforeunload', () => {
+        last_played = audios.pop()
+        if (!last_played.paused)
+            last_played.pause()
+    });
     let i = -1;
     for (const song of songinfo.songNames) {
         let li = document.createElement("li")
         let button = document.createElement("button")
         i++;
-        button.innerHTML = `<img src="music.svg"><p>${song.replace(".ogg", "")}</p><img src="play2.svg"></img>`
+        button.innerHTML = `<img src="music.svg"><p>${song.replace(".mp3", "")}</p><img src="play2.svg"></img>`
         button.dataset.url = songinfo.songs[i]
 
         button.onclick = () => {
@@ -170,6 +177,7 @@ async function main() {
             playbtn.childNodes[0].src = "play.svg"
         }
         li.appendChild(button)
+        newElements.push(li)
         songUL.appendChild(li)
     }
     let seekbar = document.querySelector(".seekbar")
@@ -177,4 +185,55 @@ async function main() {
         slide(e.offsetX, seekbar.offsetWidth);
     })
 }
-main()
+
+async function addevents() {
+    let cards1 = document.querySelectorAll(".artist-card")
+    let cards2 = document.querySelectorAll(".playlist-card")
+    for (const card of cards1) {
+        // Add event listeners for mouseover and mouseout
+        card.addEventListener('mouseover', () => {
+            const playButton = card.querySelector('.play');
+            playButton.style.opacity = '1'
+            card.classList.add("hovered")
+        });
+        card.addEventListener('mouseout', () => {
+            const playButton = card.querySelector('.play');
+            playButton.style.opacity = '0'
+            card.classList.remove("hovered")
+        });
+
+        card.addEventListener('click', async () => {
+            let artist = (card.childNodes[5].innerText).toLowerCase();
+            let songinfo = await getSongs(`./playlists/${artist}-songs/`);
+            for (const li of newElements) {
+                li.remove();
+            }
+            main(songinfo);
+        })
+    }
+    for (const card of cards2) {
+        // Add event listeners for mouseover and mouseout
+        card.addEventListener('mouseover', () => {
+            const playButton = card.querySelector('.play');
+            playButton.style.opacity = '1'
+            card.classList.add("hovered")
+        });
+        card.addEventListener('mouseout', () => {
+            const playButton = card.querySelector('.play');
+            playButton.style.opacity = '0'
+            card.classList.remove("hovered")
+        });
+        card.addEventListener('click', async () => {
+            let artist = (card.childNodes[5].innerText).toLowerCase();
+            let songinfo = await getSongs(`./playlists/${artist}-songs/`);
+            for (const li of newElements) {
+                li.remove();
+            }
+            main(songinfo);
+        })
+    }
+}
+
+
+addevents()
+playbarController()
